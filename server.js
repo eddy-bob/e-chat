5c77d33223d199fda376bc6e87a82c0a9acb8b67
eddy madu <eddymadu90@gmail.com>
2021-08-13 17:18:27 +0100

    database setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-
+const connectDB = require("./config/database.js");
 const socketio = require("socket.io");
 const server = http.Server(app);
 socketio(server);
@@ -9,6 +9,7 @@ const colors = require("colors");
 const errorWare = require("./middlewares/error.js");
 const router = require("./router/router.js");
 const { config } = require("dotenv");
+
 config({ path: ".env" });
 if (process.env.NODE_ENV === "developement") {
   app.use(morgan("dev"));
@@ -17,7 +18,7 @@ app.use("/api/v1", router);
 app.use(errorWare);
 const port = process.env.PORT || 5000;
 const enviroment = process.env.NODE_ENV;
-
+connectDB();
 server.listen(port, () => {
   console.log(
     `server running on port ${port} and ${enviroment} enviroment`.blue
